cab881a8fd82964b4c75cf4892529c9cdff10583
vapeofdarkside-sketch <vapeofdarkside@gmail.com>
2026-02-07 22:35:01 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "packbuilder-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
