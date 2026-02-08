2bd79a045d67be0d71a27cc93e0f298b74012c48
vapeofdarkside-sketch <vapeofdarkside@gmail.com>
2026-02-08 17:28:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,4 +9,16 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => r || fetch(e.request))
   );
+  function selectAllOnFocus(e){
+  const t = e.target;
+  // iOS braucht oft ein kleines Delay, sonst markiert er nicht sauber
+  setTimeout(() => {
+    try { t.select(); } catch(_) {}
+  }, 0);
+}
+
+["focus", "click"].forEach(evt => {
+  el.series.addEventListener(evt, selectAllOnFocus);
+  el.parallel.addEventListener(evt, selectAllOnFocus);
+});
 });
